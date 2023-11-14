8994e67226f44f17960a25018ad3a68c37f549cb
GenDev0 <dev.gen.info.0@gmail.com>
2023-11-14 15:18:48 +0100

    feat: add Teacher Analytics

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,6 +1,32 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import DataCard from "./_components/data-card";
+import Chart from "./_components/chart";
+
 interface AnalyticsPageProps {}
 
-const AnalyticsPage = (props: AnalyticsPageProps) => {
-  return <div>AnalyticsPage</div>;
+const AnalyticsPage = async (props: AnalyticsPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  return (
+    <div className='p-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+        <DataCard
+          value={totalRevenue}
+          label={"Total Revenue"}
+          shouldFormat={true}
+        />
+        <DataCard value={totalSales} label={"Total Sales"} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 };
 export default AnalyticsPage;
